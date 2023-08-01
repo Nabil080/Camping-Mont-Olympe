41f7fa7b67dfcdd8e9517a8142da2f4d80c5ea99
Nabil080 <bellilanabil8@gmail.com>
2023-08-01 12:22:02 +0200

    modif nav bar, icon only

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use DateTimeZone;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,12 @@ class AdminController extends AbstractController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
+        $formatter = new \IntlDateFormatter('fr_FR', \IntlDateFormatter::FULL, \IntlDateFormatter::FULL);
+        $now = new \DateTime('now', new DateTimeZone('Europe/Paris'));
+
+
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'date' => $now,
         ]);
     }
 
