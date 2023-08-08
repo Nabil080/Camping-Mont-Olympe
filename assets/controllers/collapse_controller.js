e28beb8fe477078bd05f071d14b2d4a8dc5f0f56
Nabil080 <bellilanabil8@gmail.com>
2023-08-08 14:46:04 +0200

    faq collapse

--- assets/controllers/collapse_controller.js
+++ assets/controllers/collapse_controller.js
@@ -1,13 +1,31 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["button","content"]
+  static targets = ["button", "content"];
 
   connect() {
-    this.buttonTarget.dataset.action = "collapse#toggle"
+    this.buttonTarget.dataset.action = "collapse#toggle";
+    this.contentTarget.classList.add(
+      "hidden",
+      "scale-y-0",
+      "transition",
+      "ease-in-out",
+      "duration-300",
+      "origin-top",
+    );
+
+    // this.toggle()
   }
 
   toggle() {
-    this.contentTarget.classList.toggle('hidden')
+    if (this.contentTarget.classList.contains("scale-y-0")) {
+      this.contentTarget.classList.remove("hidden");
+      setTimeout(() =>
+        this.contentTarget.classList.replace("scale-y-0", "scale-y-100"),
+      );
+    } else {
+      this.contentTarget.classList.replace("scale-y-100", "scale-y-0"),
+        setTimeout(() => this.contentTarget.classList.add("hidden"),150);
+    }
   }
 }
