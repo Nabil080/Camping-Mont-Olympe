54482305bd23ba3b5d98e32e3e04c67dd3fa8e48
Nabil080 <bellilanabil8@gmail.com>
2023-08-10 14:31:36 +0200

    navbar logo hover

--- assets/controllers/back_to_top_controller.js
+++ assets/controllers/back_to_top_controller.js
@@ -2,11 +2,11 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    this.element.classList.add("-translate-y-full","duration-700");
-    this.element.classList.remove("hidden");
+    this.element.classList.add('translate-y-[300%]','duration-300')
+    this.element.classList.remove('hidden')
 
     document.addEventListener('scroll', () => {
-        if (window.scrollY > 400) {
+        if (window.scrollY > 800) {
             this.show();
         } else {
             this.hide();
@@ -16,11 +16,11 @@ export default class extends Controller {
 
 
   show() {
-    this.element.classList.remove("-translate-y-full");
+    this.element.classList.remove("translate-y-[300%]");
   }
 
   hide() {
-    this.element.classList.add("-translate-y-full");
+    this.element.classList.add("translate-y-[300%]");
   }
 }
 
