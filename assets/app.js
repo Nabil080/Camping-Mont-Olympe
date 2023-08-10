bb7453397ca5c922d10be0c1901a4b4403726611
Nabil080 <bellilanabil8@gmail.com>
2023-08-10 13:53:59 +0200

    sticky navbar

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,7 @@
 require("@fortawesome/fontawesome-free/css/all.min.css");
 require("@fortawesome/fontawesome-free/js/all.js");
 require("@fontsource/water-brush")
+require("@fontsource/poppins")
 // require('select2')
 
 // any CSS you import will output into a single css file (app.css in this case)
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      // sans: "Roboto",
+      sans: "Poppins",
       title: "Pacifico",
       // title: "Water brush",
     },
