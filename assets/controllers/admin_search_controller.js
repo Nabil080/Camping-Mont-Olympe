2432acb13277742ececfe0e8349edd9f19c3ed2f
Nabil080 <bellilanabil8@gmail.com>
2023-08-19 19:40:57 +0200

    filtre resultat recherche manque affichage)

--- assets/controllers/admin_search_controller.js
+++ assets/controllers/admin_search_controller.js
@@ -1,14 +1,44 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["results"];
+    static targets = ["results"];
 
-  connect() {
-    console.log(this.resultsTarget)
-  }
+    onSearchInput(e) {
+        this.searchValue = e.target.value
 
-  onSearchInput() {
+        if (this.searchValue.length > 0) this.search();
+        else this.stop();
+    }
 
-    console.log(this.searchResults)
-  }
+    search() {
+        this.resultsTarget.classList.remove("hidden");
+
+        let results = values
+            .filter((array) => array[0].toLowerCase().includes(this.searchValue.toLowerCase()))
+            .map((array) => new Result(array[0], array[1]));
+
+        this.insertResults()
+    }
+
+    stop() {
+        this.resultsTarget.classList.add("hidden");
+    }
+
+    insertResults() {
+      this.resultsTarget.innerHTML = 'slt'
+
+
+    }
 }
+
+class Result {
+    constructor(display, path) {
+        this.display = display;
+        this.path = path;
+    }
+}
+
+let values = [
+    ["Ajouter une Saison", "admin_settings_seasons_add"],
+    ["Voir les Saisons", "admin_settings_seasons"],
+];
